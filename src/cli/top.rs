c7ae7ebb7d5b9dc89dbcfc4fdfb3684c9c2cce10
Josiah Mbao <josiahmbaomc@gmail.com>
2026-04-24 14:51:31 +0300

    feat(cli): implement live loop with periodic refresh and sorting

--- src/cli/top.rs
+++ src/cli/top.rs
@@ -1,38 +1,32 @@
 use std::{thread::sleep, time::Duration};
 
-use pulse::system::process::get_processes;
-use pulse::system::snapshot::{sample_system, compute_cpu_usage};
+use pulse::system::sampler::sample_processes;
 
-pub fn run_top() {
-    let prev = sample_system();
-    sleep(Duration::from_millis(500));
-    let curr = sample_system();
+pub fn run_loop() {
+    loop {
+        // Clear terminal
+        print!("\x1B[2J\x1B[1;1H");
 
-    let cpu_map = compute_cpu_usage(&prev, &curr);
+        let mut processes = sample_processes();
 
-    let mut processes = get_processes();
+        processes.sort_by(|a, b| ){
+            b.cpu_percent
+                .partial_cmp(&a.cpu_percent)
+                .unwrap()
+        });
 
-    for p in processes.iter_mut() {
-        if let Some(cpu) = cpu_map.get(&p.pid) {
-            p.cpu_percent = *cpu;
+        println!("{:<6} {:<20} {:<10} {:<10}", "PID", "NAME", "MEM(KB)", "CPU(%)");
+
+        for p in processes.iter().take(15) {
+            println!(
+                    "{:<6} {:<20} {:<10} {:.2}",
+                    p.pid,
+                    p.name,
+                    p.memory_kb,
+                    p.cpu_percent
+                );
         }
-    }
 
-    processes.sort_by(|a, b| {
-        b.cpu_percent
-            .partial_cmp(&a.cpu_percent)
-            .unwrap()
-    });
-
-    println!("{:<6} {:<20} {:<10} {:<10}", "PID", "NAME", "MEM(KB)", "CPU(%)");
-
-    for p in processes.iter().take(15) {
-        println!(
-            "{:<6} {:<20} {:<10} {:.2}",
-            p.pid,
-            p.name,
-            p.memory_kb,
-            p.cpu_percent
-        );
+        sleep(Duration::from_secs(1));
     }
 }
