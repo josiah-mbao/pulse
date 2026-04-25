a2362b0bb489ec30a45f10fd179452310d93c97b
Josiah Mbao <josiahmbaomc@gmail.com>
2026-04-25 16:03:14 +0300

    refactor: decouple CLI from system logic in top command

--- src/cli/top.rs
+++ src/cli/top.rs
@@ -1,32 +1,5 @@
-use std::{thread::sleep, time::Duration};
-
-use pulse::system::sampler::sample_processes;
+use crate::system::engine::run_top_loop;
 
 pub fn run_top() {
-    loop {
-        // Clear terminal
-        print!("\x1B[2J\x1B[1;1H");
-
-        let mut processes = sample_processes();
-
-        processes.sort_by(|a, b| {
-            b.cpu_percent
-                .partial_cmp(&a.cpu_percent)
-                .unwrap()
-        });
-
-        println!("{:<6} {:<20} {:<10} {:<10}", "PID", "NAME", "MEM(KB)", "CPU(%)");
-
-        for p in processes.iter().take(15) {
-            println!(
-                    "{:<6} {:<20} {:<10} {:.2}",
-                    p.pid,
-                    p.name,
-                    p.memory_kb,
-                    p.cpu_percent
-                );
-        }
-
-        sleep(Duration::from_secs(1));
-    }
+    run_top_loop();
 }
