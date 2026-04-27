a2c2d0248b837179ed0ac8a442d8dda781b7439e
Josiah Mbao <josiahmbaomc@gmail.com>
2026-04-27 21:51:19 +0300

    feat: introduce AppState for TUI state management

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -1,10 +1,39 @@
 use std::io;
+use std::collections::HashMap;
+
+use crate::system::state::ProcessSnapshot;
 
 use crossterm::{
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode},
 };
 use ratatui::{backend::CrosstermBackend, Terminal};
+
+
+#[derive(Clone, Copy)]
+pub enum SortMode {
+    Cpu,
+    Memory,
+}
+
+pub struct AppState {
+    pub processes: HashMap<u32, ProcessSnapshot>,
+    pub cpu_map: HashMap<u32, f32>,
+    pub sort_mode: SortMode,
+    pub paused: bool,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self {
+            processes: HashMap::new(),
+            cpu_map: HashMap::new(),
+            sort_mode: SortMode::Cpu,
+            paused: false,
+        }
+    }
+}
+
 
 pub fn run_app() -> Result<(), io::Error> {
     enable_raw_mode()?;
